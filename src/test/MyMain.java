454fcefcdc584978f3890d210ee78df6161f44f8
dns-007 <dns-007@gmx.de>
2019-01-16 16:50:42 +0100

    Feature4

--- src/test/MyMain.java
+++ src/test/MyMain.java
@@ -11,6 +11,8 @@ public class MyMain {
 		System.out.println("Hello my World!");
 		System.out.println("This is feature 2!");
 		System.out.println("This is feature 3!");
+		
+		System.out.println("This is feature 4!");
 
 	}
 
