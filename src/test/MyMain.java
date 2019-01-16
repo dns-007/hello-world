8836ee36c419618536d1820842b347b8cce1711e
jw <jw@jw-PC>
2019-01-16 15:33:58 +0100

    added feature 3

--- src/test/MyMain.java
+++ src/test/MyMain.java
@@ -7,7 +7,7 @@ public class MyMain {
 		System.out.println("This is feature1");
 		System.out.println("Hello World!!!");
 		System.out.println("This is feature 2!");
-
+		System.out.println("This is feature 3!");
 
 	}
 
