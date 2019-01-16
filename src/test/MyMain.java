579fdff8526f2fcf61c578217b6dc64cb4174228
dns-007 <dns-007@gmx.de>
2019-01-16 15:39:00 +0100

    my world change

--- src/test/MyMain.java
+++ src/test/MyMain.java
@@ -7,7 +7,7 @@ public class MyMain {
 		System.out.println("Master Release");
 		
 		System.out.println("This is feature1");
-		System.out.println("Hello World!!!");
+		System.out.println("Hello my World!!!");
 		System.out.println("This is feature 2!");
 		System.out.println("This is feature 3!");
 
